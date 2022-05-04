d2001d7278e4bf113c118caca364f337647da374
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-05 00:09:45 +0900

    Initial commit

--- W06/w06_task1_main.js
+++ W06/w06_task1_main.js
@@ -6,7 +6,7 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/W04/data.csv")
             parent: '#drawing_region',
             width: 256,
             height: 256,
-            margin: {top:50, right:30, bottom:20, left:80}
+            margin: {top:50, right:30, bottom:40, left:80}
             //margin: {top:10, right:10, bottom:20, left:30}
         };
 
@@ -24,7 +24,7 @@ class ScatterPlot {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:50, right:30, bottom:20, left:80}
+            margin: config.margin || {top:50, right:30, bottom:40, left:80}
             //margin: config.margin || {top:10, right:10, bottom:10, left:10}
         }
         this.data = data;
@@ -56,10 +56,12 @@ class ScatterPlot {
             .range( [0, self.inner_height] );
 
         self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(7);
+            .ticks(7)
+            .tickPadding(10);
 
         self.yaxis = d3.axisLeft( self.yscale )
-            .ticks(7);
+            .ticks(7)
+            .tickPadding(10);
 
         self.xaxis_group = self.chart.append('g')
             //.attr('transform', `translate(0, ${self.inner_height})`);
@@ -110,7 +112,7 @@ class ScatterPlot {
             .call( self.xaxis )
             .append("text")
             .attr("fill", "black")
-			.attr("x", 30)
+			.attr("x", 50)
 			.attr("y", 30)
             .attr("font-size", "10pt")
             .attr("font-weight", "bold")
