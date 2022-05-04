f064cdfeca3709f62783e3dd621c778ecc8bc7f9
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-04 20:42:13 +0900

    Initial commit

--- W06/w06_task1_main.js
+++ W06/w06_task1_main.js
@@ -45,7 +45,7 @@ class ScatterPlot {
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
         self.axis_inner_width = self.inner_width - 30;
-        self.axis_inner_height = self.inner_height + 30;
+        self.axis_inner_height = self.inner_height - 30;
 
         self.xscale = d3.scaleLinear()
             //.range( [0, self.inner_width] );
