74ce0b13c3be21376d6b3faa6108e84d8da82457
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-11 14:40:54 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -1,63 +1,96 @@
-/*var data = [
-    {label:'Apple', value:100},
-    {label:'Banana', value:200},
-    {label:'Cookie', value:50},
-    {label:'Doughnut', value:120},
-    {label:'Egg', value:80}
-];*/
+d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
-//var width = 256;
-var width = 1500;
-//var height = 128;
-var height = 1500;
-var margin = {top:10, right:10, bottom:20, left:60};
-const inner_width = width - margin.left - margin.right;
-const inner_height = height - margin.top - margin.bottom;
+        var config = {
+            parent: '#drawing_region',
+            width: 1500,
+            height: 1500,
+            margin: {top:10, right:10, bottom:20, left:60}
+        };
 
-d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv", function(error, data) {
-    svg.select('#drawing_region')
-    .attr('width', width)
-    .attr('height', height);
+        const barchart_plot = new BarChart( config, data );
+        barchart_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
 
-var chart = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    class ScatterPlot {
 
+        constructor( config, data ) {
+            this.config = {
+                parent: config.parent,
+                width: config.width || 1500,
+                height: config.height || 1500,
+                margin: config.margin || {top:10, right:10, bottom:20, left:60}
+            }
+            this.data = data;
+            this.init();
+        }
+    
+        init() {
+            let self = this;
+    
+            self.svg = d3.select( self.config.parent )
+                .attr('width', self.config.width)
+                .attr('height', self.config.height);
+    
+            self.chart = self.svg.append('g')
+                .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+    
+            self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+            self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+    
+            self.xscale = d3.scaleLinear()
+            .range([0, self.inner_width]);
+    
+            self.yscale = d3.scaleLinear()
+            .range([0, self.inner_height])
+            .paddingInner(0.1);
+    
+            self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(5)
+            .tickSizeOuter(0);
+    
+            self.yaxis = d3.axisLeft( self.yscale )
+            .tickSizeOuter(0);
+    
+            self.xaxis_group = self.chart.append('g')
+                .attr('transform', `translate(0, ${self.inner_height })`);
+    
+            self.yaxis_group = self.chart.append('g')
+        }
+    
+        update() {
+            let self = this;
 
+            self.xscale.domain([0, d3.max(self.data, d => d.value)])
 
-// Initialize axis scales
-const xscale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.value)])
-      .range([0, inner_width]);
+            self.yscale.domain(self.data.map(d => d.label))
+    
+            self.render();
+        }
+    
+        render() {
 
-const yscale = d3.scaleBand()
-      .domain(data.map(d => d.label))
-      .range([0, inner_height])
-      .paddingInner(0.1);
+            let self = this;
 
-// Initialize axes
-const xaxis = d3.axisBottom( xscale )
-      .ticks(5)
-      .tickSizeOuter(0);
-
-const yaxis = d3.axisLeft( yscale )
-      .tickSizeOuter(0);
-
-// Draw the axis
-const xaxis_group = chart.append('g')
-      .attr('transform', `translate(0, ${inner_height})`)
-      .call( xaxis );
-
-const yaxis_group = chart.append('g')
-      .call( yaxis );
-
-// Draw bars
-chart.selectAll("rect").data(data).enter()
-    .append("rect")
+            self.chart.selectAll("rect")
+            .data(self.data)
+            .enter()
+            .append("rect")
     .attr("x", 0)
-    //.attr("y", d => yscale(d.都道府県名))
     .attr("y", d => yscale(d.label))
-    //.attr("width", d => xscale(d.人口2015年))
     .attr("width", d => xscale(d.value))
     .attr("height", yscale.bandwidth());
 
-});
+    self.xaxis_group
+    .call( self.xaxis );
+
+    self.yaxis_group
+            .call( self.yaxis );
+
+        }
+    }
+
