3d5ac3b6f517fff13fcbf307c4d5ecf0a1bb208e
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-11 13:58:49 +0900

    Initial commit

---
+++ W08/w08_task1_main.js
@@ -0,0 +1,59 @@
+/*var data = [
+    {label:'Apple', value:100},
+    {label:'Banana', value:200},
+    {label:'Cookie', value:50},
+    {label:'Doughnut', value:120},
+    {label:'Egg', value:80}
+];*/
+
+d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv", function(data){});
+
+//var width = 256;
+var width = 1500;
+//var height = 128;
+var height = 1500;
+var margin = {top:10, right:10, bottom:20, left:60};
+
+var svg = d3.select('#drawing_region')
+    .attr('width', width)
+    .attr('height', height);
+
+var chart = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+const inner_width = width - margin.left - margin.right;
+const inner_height = height - margin.top - margin.bottom;
+
+// Initialize axis scales
+const xscale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.人口2015年)])
+      .range([0, inner_width]);
+
+const yscale = d3.scaleBand()
+      .domain(data.map(d => d.都道府県名))
+      .range([0, inner_height])
+      .paddingInner(0.1);
+
+// Initialize axes
+const xaxis = d3.axisBottom( xscale )
+      .ticks(5)
+      .tickSizeOuter(0);
+
+const yaxis = d3.axisLeft( yscale )
+      .tickSizeOuter(0);
+
+// Draw the axis
+const xaxis_group = chart.append('g')
+      .attr('transform', `translate(0, ${inner_height})`)
+      .call( xaxis );
+
+const yaxis_group = chart.append('g')
+      .call( yaxis );
+
+// Draw bars
+chart.selectAll("rect").data(data).enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", d => yscale(d.都道府県名))
+    .attr("width", d => xscale(d.人口2015年))
+    .attr("height", yscale.bandwidth());
