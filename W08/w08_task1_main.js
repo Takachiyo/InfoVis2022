baaa26c3ff16c2f68fad3eef918aed8fe641b8a7
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-11 14:09:02 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -6,7 +6,9 @@
     {label:'Egg', value:80}
 ];*/
 
-d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv", function(data){});
+d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv", function(error, data) {
+    console.log(data);
+});
 
 //var width = 256;
 var width = 1500;
