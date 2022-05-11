127cf0e11e95611cea4f97e6b1e42207ede979a3
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-11 16:11:41 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -1,5 +1,5 @@
 d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv",function(error, data){
-    console.log(data);
+    console.table(data);
     })
     .catch( error => {
         console.log( error );
