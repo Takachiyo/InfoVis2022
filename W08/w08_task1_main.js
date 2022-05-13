6c25f31fb718ad639775b7594b5db50a74c62efe
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-13 11:17:22 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -7,4 +7,6 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv", 
 d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv")
     .then( data => {
         data.forEach( d => { d.value = +d.value; });
+
+        console.log(data)
     });
