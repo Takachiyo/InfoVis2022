c439c4766f35e287798dd1e2044098ec30e6e80e
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-13 10:42:54 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -1,9 +1,3 @@
-d3.csv("https://takachiyo.github.io/InfoVis2022/W08/resource.csv", function(error, data){
-    // エラー処理
-    if(error != null){
-      return;
-    }  
-    // 通常処理（dataを使用してグラフを描画）
+d3.csv("https://takachiyo.github.io/InfoVis2022/W08/data.csv", function(error, data) {
     console.log(data);
-
-  });
+});
