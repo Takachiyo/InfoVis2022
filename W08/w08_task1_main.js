6f118da7454ad8ade6cb0a219bdb4090a8a3d61a
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-13 11:31:52 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -49,11 +49,11 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv")
             self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
     
             self.xscale = d3.scaleLinear()
-            //.domain([0, d3.max(self.data, d => d.value)])
+            .domain([0, d3.max(self.data, d => d.value)])
             .range([0, self.inner_width]);
     
             self.yscale = d3.scaleBand()
-            //.domain(self.data.map(d => d.label))
+            .domain(self.data.map(d => d.label))
             .range([0, self.inner_height])
             .paddingInner(0.1);
     
@@ -91,7 +91,7 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv")
             .attr("x", 0)
             .attr("y", d => yscale(d.label))
             .attr("width", d => xscale(d.value))
-            .attr("height", yscale.bandwidth());
+            .attr("height", self.yscale.bandwidth());
 
             self.xaxis_group
             .call( self.xaxis );
