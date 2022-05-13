80752a7e8c60da08a5b9c08f4f1ee85d97e710cc
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-13 10:53:48 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -1,8 +1,3 @@
 d3.csv("https://takachiyo.github.io/InfoVis2022/W08/data.csv", function(error, data) {
-
+console.table(data);
 });
-
-const allData = data.map(data=> {
-    return data
-    });
-    console.table(allData);
