c7964cd6bcbe6a30529795cf855548ad16bd2039
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-13 11:33:10 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -89,8 +89,8 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/W04/csv_pop_estimate_2045.csv")
             .enter()
             .append("rect")
             .attr("x", 0)
-            .attr("y", d => yscale(d.label))
-            .attr("width", d => xscale(d.value))
+            .attr("y", d => self.yscale(d.label))
+            .attr("width", d => self.xscale(d.value))
             .attr("height", self.yscale.bandwidth());
 
             self.xaxis_group
