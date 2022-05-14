e002c2286010fd3403b00355f6517832b5f9582c
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-14 20:47:03 +0900

    Initial commit

--- W08/w08_task2_main.js
+++ W08/w08_task2_main.js
@@ -1,6 +1,6 @@
-d3.csv("https://takachiyo.github.io/InfoVis2022/W08/data1.csv")
-    .then( data => {
-        data.forEach( d => { d.value = +d.value; });
+d3.csv("https://takachiyo.github.io/InfoVis2022/W08/data2.csv")
+.then( data => {
+    data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
         var config = {
             parent: '#drawing_region',
