5671b7de8e926fbb50ea28f8b4f1bdae6de1a4bc
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-15 01:06:24 +0900

    Initial commit

--- W08/w08_task0_main.js
+++ W08/w08_task0_main.js
@@ -8,10 +8,40 @@ var data = [
 
 var width = 256;
 var height = 128;
+var margin = {top:10, right:10, bottom:20, left:60};
 
 var svg = d3.select('#drawing_region')
     .attr('width', width)
     .attr('height', height);
+
+const inner_width = width - margin.left - margin.right;
+const inner_height = height - margin.top - margin.bottom;
+
+// Initialize axis scales
+const xscale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.value)])
+      .range([0, inner_width]);
+
+const yscale = d3.scaleBand()
+      .domain(data.map(d => d.label))
+      .range([0, inner_height])
+      .paddingInner(0.1);
+
+// Initialize axes
+const xaxis = d3.axisBottom( xscale )
+      .ticks(5)
+      .tickSizeOuter(0);
+
+const yaxis = d3.axisLeft( yscale )
+      .tickSizeOuter(0);
+
+// Draw the axis
+const xaxis_group = chart.append('g')
+      .attr('transform', `translate(0, ${inner_height})`)
+      .call( xaxis );
+
+const yaxis_group = chart.append('g')
+      .call( yaxis );
 
 const line = d3.line()
       .x( d => d.x )
