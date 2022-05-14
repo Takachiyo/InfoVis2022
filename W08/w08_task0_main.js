6a8a50972dec20e01085061da2ed346d3cfb74c6
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-15 01:33:26 +0900

    Initial commit

--- W08/w08_task0_main.js
+++ W08/w08_task0_main.js
@@ -25,10 +25,9 @@ const xscale = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.value)])
       .range([0, inner_width]);
 
-const yscale = d3.scaleBand()
+const yscale = d3.scaleLinear()
       .domain(data.map(d => d.label))
       .range([0, inner_height])
-      .paddingInner(0.1);
 
 // Initialize axes
 const xaxis = d3.axisBottom( xscale )
