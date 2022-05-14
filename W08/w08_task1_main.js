37866c16a83568877391f9f91762eb5f270a30ec
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-14 13:46:50 +0900

    Initial commit

--- W08/w08_task1_main.js
+++ W08/w08_task1_main.js
@@ -4,8 +4,8 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/W08/data.csv")
 
         var config = {
             parent: '#drawing_region',
-            width: 256,
-            height: 128,
+            width: 1000,
+            height: 500,
             margin: {top:80, right:10, bottom:20, left:80}
         };
 
