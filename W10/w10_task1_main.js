4e48efa84873f885fd859dae051e147ff92955f9
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-19 09:29:19 +0900

    Initial commit

--- W10/w10_task1_main.js
+++ W10/w10_task1_main.js
@@ -12,17 +12,16 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/W10/data1.csv")
         const barchart_plot = new BarChart( config, data );
         barchart_plot.update();
 
-        
-    })
-    .catch( error => {
-            console.log( error );
-    });
-
-    d3.select('#reverse')
-    .on('click', d => {
+        d3.select('#reverse')
+        .on('click', d => {
         data.reverse();
         const barchart_plot2 = new BarChart( config, data );
         barchart_plot2.update();
+    });
+    
+    })
+    .catch( error => {
+            console.log( error );
     });
 
     class BarChart {
