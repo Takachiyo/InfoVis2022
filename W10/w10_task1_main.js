2542acbae4329f770b23000884a86072dc7aecaf
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-22 15:50:19 +0900

    Initial commit

--- W10/w10_task1_main.js
+++ W10/w10_task1_main.js
@@ -1,24 +1,120 @@
-
-
 d3.csv("https://takachiyo.github.io/InfoVis2022/W10/data1.csv")
     .then( data => {
         data.forEach( d => { d.value = +d.value; });
-        update( data );       
-    
+
+        var config = {
+            parent: '#drawing_region',
+            width: 1000,
+            height: 500,
+            margin: {top:60, right:10, bottom:50, left:80}
+        };
+
+        const barchart_plot = new BarChart( config, data );
+        barchart_plot.update();
     })
     .catch( error => {
             console.log( error );
     });
 
-    function update(data) {
-        let padding = 10;
-        let height = 20
-        
-        d3.select('#drawing_region').selectAll("rect")
-        .data(data)
-        .join("rect")
-        .attr("x", padding)
-        .attr("y", (d,i) => padding + i * ( height + padding ))
-        .attr("width", d => d)
-        .attr("height", height);
+    class BarChart {
+
+        constructor( config, data ) {
+            this.config = {
+                parent: config.parent,
+                width: config.width || 1000,
+                height: config.height || 500,
+                margin: config.margin || {top:60, right:10, bottom:50, left:80}
+            }
+            this.data = data;
+            this.init();
+        }
+    
+        init() {
+            let self = this;
+    
+            self.svg = d3.select( self.config.parent )
+                .attr('width', self.config.width)
+                .attr('height', self.config.height)
+                .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+    
+            self.chart = self.svg.append('g')
+                .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+    
+            self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+            self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+    
+            self.xscale = d3.scaleLinear()
+            .range([0, self.inner_width]);
+    
+            self.yscale = d3.scaleBand()
+            .range([0, self.inner_height])
+            .paddingInner(0.1);
+    
+            self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(5)
+            .tickSizeOuter(0);
+    
+            self.yaxis = d3.axisLeft( self.yscale )
+            .tickSizeOuter(0);
+    
+            self.xaxis_group = self.chart.append('g')
+                .attr('transform', `translate(0, ${self.inner_height})`);
+    
+            self.yaxis_group = self.chart.append('g')
+            //.attr('transform', `translate(0, 30)`);
+        }
+    
+        update() {
+            let self = this;
+
+            self.xscale.domain([0, d3.max(self.data, d => d.value)])
+
+            self.yscale.domain(self.data.map(d => d.label))
+    
+            self.render();
+        }
+    
+        render() {
+
+            let self = this;
+
+            self.svg.append("text")
+            .attr("fill", "black")
+			.attr("x", 200)
+			.attr("y", 25)
+            .attr("font-size", "20pt")
+            .attr("font-weight", "bold")
+            .text("2015年度 近畿地方の人口");
+
+            self.chart.selectAll("rect")
+            .data(self.data)
+            .enter()
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", d => self.yscale(d.label))
+            .attr("width", d => self.xscale(d.value))
+            .attr("height", self.yscale.bandwidth());
+
+            self.xaxis_group
+            .call( self.xaxis )
+            .append("text")
+            .attr("fill", "black")
+			.attr("x", 410)
+			.attr("y", 40)
+            .attr("font-size", "10pt")
+            .attr("font-weight", "bold")
+            .text("人口数");
+            
+            self.yaxis_group
+            .call( self.yaxis )
+            .append("text")
+            .attr("fill", "black")
+			.attr("x", -150)
+			.attr("y", -55)
+            .attr("transform", "rotate(-90)")
+            .attr("font-size", "10pt")
+            .attr("font-weight", "bold")
+            .text("府県名");
+
+        }
     }
