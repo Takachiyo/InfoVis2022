0b8d843ca45d897d9be2f0908ab3fc44667675c6
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-05-23 03:24:55 +0900

    Initial commit

--- W10/w10_task2_main.js
+++ W10/w10_task2_main.js
@@ -70,11 +70,11 @@ class ScatterPlot {
 
         const xmin = d3.min( self.data, d => d.x );
         const xmax = d3.max( self.data, d => d.x );
-        self.xscale.domain( [0, xmax+20] );
+        self.xscale.domain( [xmin, xmax] );
 
         const ymin = d3.min( self.data, d => d.y );
         const ymax = d3.max( self.data, d => d.y );
-        self.yscale.domain( [ymax+10, 0] );
+        self.yscale.domain( [ymax, ymin] );
 
         self.render();
     }
