d41cbb6d7ba22681a7c96c3370b040cc87e970c4
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-10 11:55:15 +0900

    Initial commit

--- Final/final_main.js
+++ Final/final_main.js
@@ -1,5 +1,22 @@
-d3.json("japan.json").then(function(json) {
+//画面の幅と高さ
+var w = 600;
+var h = 600;
+var scale = 1200; //地図のスケール
  
+var projection = d3.geoMercator()
+   .center([ 136.0, 35.6 ])
+   .translate([w/2, h/2])
+   .scale(scale);
+
+var path = d3.geoPath().projection(projection);
+
+var svg = d3.select("body")
+         .append("svg")
+         .attr("width", w)
+         .attr("height", h);
+
+d3.json("japan.json").then(function(json) {
+
     svg.selectAll("path")   //都道府県の領域データをpathで描画
        .data(json.features)
        .enter()
