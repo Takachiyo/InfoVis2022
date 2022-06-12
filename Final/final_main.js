d55ee4cb45c8b850f60de12b6e80c064295ba8b2
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-12 11:10:43 +0900

    Initial commit

--- Final/final_main.js
+++ Final/final_main.js
@@ -13,7 +13,7 @@ var projection = d3.geoMercator()
 var path = d3.geoPath(projection);
 
 //d3.csv("日平均気温(2019).csv", function(data) {
-   d3.json("japan.json").then(function(json) {
+   d3.json("https://takachiyo.github.io/InfoVis2022/Final/japan.geojson").then(function(json) {
       svg.append("g").selectAll("path")
           .data(json.features)
           .enter()
