156ae016cf386e76e5856ef3e4a525ffe629647d
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-12 14:01:30 +0900

    Initial commit

--- Final/final_main.js
+++ Final/final_main.js
@@ -18,14 +18,14 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/Final/日平均気温(2019).csv"
    data.forEach( d => { d.value = +d.value; });
 
    var colorScale = d3.scaleLinear()
-               .domain([d3.min(data, function(d) { return parseInt(d.value);})
-                  , d3.max(data, function(d) { return parseInt(d.value);})])
+               .domain([d3.min(data, function(d) { return parseInt(d.temperature);})
+                  , d3.max(data, function(d) { return parseInt(d.temperature);})])
                .range([100, 50]);
 
    d3.json("https://takachiyo.github.io/InfoVis2022/Final/japan.geojson").then(function(json) {
       for (var i = 0; i < data.length; i++) {
          var dataPref = data[i].area;            //都道府県の名前を取得
-         var dataPopulation = parseFloat(data[i].value);   //人口データを数値変換
+         var dataTemperature = parseFloat(data[i].temperature);   //人口データを数値変換
 
          //GeoJSONのデータの中で同じ都道府県名を検索
          for (var j = 0; j < json.features.length; j++) {
@@ -35,7 +35,7 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/Final/日平均気温(2019).csv"
             if (dataPref == jsonPref) {
 
                //見つけたら、JSONデータに人口データをコピー
-               json.features[j].properties.value = dataPopulation;
+               json.features[j].properties.temperature = dataTemperature;
 
                //ループを抜ける
                break;
@@ -52,7 +52,7 @@ d3.csv("https://takachiyo.github.io/InfoVis2022/Final/日平均気温(2019).csv"
       .style("stroke", "gray")
       .style("stroke-width", 0.25)
       .style("fill", function(d){
-         return "hsl(0, 100%, " + (colorScale(d.properties.value)) + "%)";
+         return "hsl(0, 100%, " + (colorScale(d.properties.temperature)) + "%)";
          //return "hsl(0, " + (colorScale(d.properties.population)) + "%, 50%)";
          //return colorScale(d.properties.value);
        });
