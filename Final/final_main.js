a7fe01f906cf375b4b3cf8aa9f1f81760267d2a6
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-12 10:33:48 +0900

    Initial commit

--- Final/final_main.js
+++ Final/final_main.js
@@ -7,43 +7,18 @@ var svg = d3.select("body")
 .attr("height", height);
 
 var projection = d3.geoMercator()
-.center([136, 35.5])
-  //.rotate([210, 0])
-  //.parallels([50, 60])
-  .translate([w/2, h/2])
-  .scale([1500]);
+      .center([131.00,32.26])
+      .scale(100000)
+
 var path = d3.geoPath(projection);
 
 d3.csv("日平均気温(2019).csv", function(data) {
-  d3.json("japan.json", function(json) {
-    for(var i=0; i<data.length; i++) {
-      for(var j=0; j<json.features.length; j++) {
-        if( data[i].都道府県名 == json.features[j].properties.name_local ) {
-          json.features[j].properties.日平均気温 = data[i].日平均気温;
-        }
-      }
-    }
-
-    svg.selectAll("path")
-      .data(json.features)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .style("fill", function(feat) {
-        var 日平均気温 = feat.properties.日平均気温;
-        if( 日平均気温 > 4000 )
-          var c = "darkred";
-        else if( p日平均気温 > 3000 )
-          var c = "orangered";
-        else if( 日平均気温 > 2500 )
-          var c = "orange";
-        else if( 日平均気温 > 2250 )
-          var c = "gold";
-        else
-          var c = "black";
-        return c;
-      })
-      .style("stroke", "gray")
-      .style("stroke-width", "0.5px");
+   d3.json("japan.geojson").then(function(json) {
+      svg.append("g").selectAll("path")
+          .data(json.features)
+          .enter()
+          .append("path")
+          .attr("d", path)
+          .style("fill","red") //styleの記述を追加し単色で地図を塗りつぶす
   });
 });
