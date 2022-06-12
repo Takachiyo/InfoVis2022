361f6cc066b7499d6c8b08af1e1a52ec6852ea12
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-12 17:56:51 +0900

    Initial commit

---
+++ Final/temperature_bar.js
@@ -0,0 +1,52 @@
+var width3 = 256;
+var height3 = 256;
+var margin3 = {top:10, right:10, bottom:20, left:60};
+
+var svg3 = d3.select('#drawing_region')
+    .attr('width', width3)
+    .attr('height', height3);
+
+var chart = svg3.append('g')
+    .attr('transform', `translate(${margin3.left}, ${margin3.top})`);
+
+const inner_width3 = width3 - margin.left3 - margin3.right;
+const inner_height3 = height3 - margin.top3 - margin3.bottom;
+
+d3.csv("https://takachiyo.github.io/InfoVis2022/Final/日平均気温(2019).csv")
+.then( data => {
+   data.forEach( d => { d.temperature = +d.temperature; });
+// Initialize axis scales
+const xscale3 = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.temperature)])
+      .range([0, inner_width3]);
+
+const yscale3 = d3.scaleBand()
+      .domain(data.map(d => d.label))
+      .range([0, inner_height3])
+      .paddingInner(0.1);
+
+// Initialize axes
+const xaxis3 = d3.axisBottom( xscale3 )
+      .ticks(5)
+      .tickSizeOuter(0);
+
+const yaxis3 = d3.axisLeft( yscale3 )
+      .tickSizeOuter(0);
+
+// Draw the axis
+const xaxis_group3 = chart.append('g')
+      .attr('transform', `translate(0, ${inner_height3})`)
+      .call( xaxis3 );
+
+const yaxis_group3 = chart.append('g')
+      .call( yaxis3 );
+
+// Draw bars
+chart.selectAll("rect").data(data).enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", d => yscale(d.label))
+    .attr("width", d => xscale(d.temperature))
+    .attr("height", yscale.bandwidth());
+
+});
