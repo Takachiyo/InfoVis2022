acd9b19eadf4fce18ed1e824b9adb2e404f3aaee
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-12 11:50:19 +0900

    Initial commit

--- Final/final_main.js
+++ Final/final_main.js
@@ -16,12 +16,48 @@ var path = d3.geoPath().projection(projection);
 d3.csv("https://takachiyo.github.io/InfoVis2022/Final/日平均気温(2019).csv")
 .then( data => {
    data.forEach( d => { d.value = +d.value; });
-   
+
+   var colorScale = d3.scaleLinear()
+               .domain([d3.min(data, function(d) { return parseInt(d.value);})
+                  , d3.max(data, function(d) { return parseInt(d.value);})])
+               .range([100, 50]);
+
+
+
    d3.json("https://takachiyo.github.io/InfoVis2022/Final/japan.geojson").then(function(json) {
-      svg.append("g").selectAll("path")
-          .data(json.features)
-          .enter()
-          .append("path")
-          .attr("d", path)
-  });
+      
+      for (var i = 0; i < data.length; i++) {
+
+         var dataPref = data[i].area;            //都道府県の名前を取得
+         var dataPopulation = parseFloat(data[i].value);   //人口データを数値変換
+
+         //GeoJSONのデータの中で同じ都道府県名を検索
+         for (var j = 0; j < json.features.length; j++) {
+
+            var jsonPref = json.features[j].properties.pref_j;
+
+            if (dataPref == jsonPref) {
+
+               //見つけたら、JSONデータに人口データをコピー
+               json.features[j].properties.value = dataPopulation;
+
+               //ループを抜ける
+               break;
+            }
+         }
+      }
+   
+   
+   svg.selectAll("path")   //都道府県の領域データをpathで描画
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "gray")
+      .style("stroke-width", 0.25)
+      .style("fill", function(d){
+         return "hsl(0, 100%, " + (colorScale(d.properties.population)) + "%)";
+         //return "hsl(0, " + (colorScale(d.properties.population)) + "%, 50%)";
+       });
+   });
 });
