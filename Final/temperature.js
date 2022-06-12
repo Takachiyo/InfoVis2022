1a1fb67cdf73c58206a69d6c6079555b5c731d5c
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-12 19:21:01 +0900

    Initial commit

--- Final/temperature.js
+++ Final/temperature.js
@@ -1,5 +1,5 @@
 var width = 400;
-var height = 400;
+var height = 600;
 
 var svg = d3.select("body")
 .append("svg")
@@ -8,7 +8,7 @@ var svg = d3.select("body")
 
 var projection = d3.geoMercator()
       .center([ 136.0, 35.6 ])
-      .translate([width/2, 50+height/2])
+      .translate([200, 250])
       .scale(1000)
 
 var path = d3.geoPath().projection(projection);
