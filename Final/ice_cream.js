8df14715e288ecbe84ae8ea3150f6871b011422a
Takachiyo <221x111x@stu.kobe-u.ac.jp>
2022-06-12 15:25:47 +0900

    Initial commit

--- Final/ice_cream.js
+++ Final/ice_cream.js
@@ -8,7 +8,7 @@ var svg = d3.select("body")
 
 var projection = d3.geoMercator()
       .center([ 136.0, 35.6 ])
-      .translate([600+(width/2), height/2])
+      .translate([width/2, height/2])
       .scale(1200)
 
 var path = d3.geoPath().projection(projection);
